42b8de1d86d09ceeaec9b48b53365dad326db505
Khadija-Khairullah <kkhairul@uwo.ca>
2017-05-10 15:03:59 -0400

    commit2 in Source1

--- test/Source1.cpp
+++ test/Source1.cpp
@@ -1,2 +1,5 @@
 //let's see what happens if we add another file in master
 //it would have been nice to do it in a new branch, but we need to commit these changes now
+
+//now we can just make changes in this file to make a new commit 
+//note that in the changes tab it will list all of the files where changes are made before a commit
