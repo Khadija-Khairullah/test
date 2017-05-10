0d71665d320bfdcb9aac7c4b264ca9384ad22885
Khadija-Khairullah <kkhairul@uwo.ca>
2017-05-10 13:40:09 -0400

    commit with notes on how to use github

--- test/Source.cpp
+++ test/Source.cpp
@@ -5,3 +5,16 @@ int manin()
 {
 	cout << "Hello World" << endl;
 }
+
+//notes on How github in visual studio works
+//must have an existing repo on github online
+//to make a repo, it must be in the file on the computer that has all repos
+//save the vs project in this file
+//in solution explorer, right click on the solution and click "Add Solution to Source Control"
+//to save changes and create a commit, go to team explorer->changes
+//write a description and commit all
+//for the first commit, it will promt you to enter the url of the repo from the online github
+//it will prompt you to sync, which will update the repo online
+//for all following commits, eery commit needs to be synced and then pushed
+
+
